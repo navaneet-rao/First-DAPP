56780750c4e49151f8261da74163f03a6f890b1a
Navaneet r rao <76439641+navaneet-rao@users.noreply.github.com>
2022-12-11 12:19:16 +0530

    web3 added

--- my-dapp/src/App.js
+++ my-dapp/src/App.js
@@ -1,17 +1,41 @@
-import "./App.css";
-import * as React from 'react';
-// import { render } from 'react-dom';
+import Web3 from "web3";
+import React, { Component } from "react";
 // import SvelteComponent from 'react-svelte';
 // import Header from "./components/Header.svelte";
 // import Chat from "./components/Chat.svelte";
-function App() {
-  return (
-    <div className="App">
-      
-      {/* <Header /> */}
-      {/* <Chat /> */}
-    </div>
-  );
+
+class App extends Component {
+  componentWillMount() {
+    this.loadBlockchainData();
+  }
+
+  async loadBlockchainData() {
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8535");
+    const network = await web3.eth.net.getNetworkType();
+    console.log("network:", network);
+  }
+  render() {
+    return (
+      <div className="containers">
+        <h1>Hello World !!!</h1>
+      </div>
+    );
+  }
 }
+
+// function App() {
+//   async loadBlockchainData() {
+//      const web3 = new Web3
+//   }
+//   return (
+//     <div className="=container">
+//       {/* <Header /> */}
+//       {/* <Chat /> */}
+
+//       <h1>Hello World!!!!!</h1>
+
+//     </div>
+//   );
+// }
 
 export default App;
